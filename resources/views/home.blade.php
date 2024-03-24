e6272705940bfeb9815c92e7628d33299d7a3811
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-24 23:59:12 +0100

    Feat home.blade.php - print films

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,23 @@
 @section('main-content')
   <section>
     <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
+      <h1>Films</h1>
+
+      <ul>
+        @forelse ($movies as $movie)
+        <li>ID: {{ $movie['id'] }}</li>
+        <li>Titolo: {{ $movie['title'] }}</li>
+        <li>Titolo originale: {{ $movie['original_title'] }}</li>
+        <li>Lingua: {{ $movie['nationality'] }}</li>
+        <li>Data: {{ $movie['date'] }}</li>
+        <li>Voto: {{ $movie['vote'] }}</li>
+      </ul>
+      <hr>
+    @empty
+    <div class="alert alert-warning">
+      Nessun film selezionato
+    </div>
+    @endforelse
     </div>
   </section>
 @endsection
